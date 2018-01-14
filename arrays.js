53f37e95d526c6edaa1b64cd3611fec32da553eb
Jason Lesslie <jason.lesslie@gmail.com>
2018-01-14 02:04:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,8 @@
-function addElementToBeginningOfArray() {
-  
+function addElementToBeginningOfArray(originArray, addElement) {
+  var outputArray = originArray.unshift(addElement)
+  return outputArray
 }
-function destructivelyAddElementToBeginningOfArray() {
-  
+function destructivelyAddElementToBeginningOfArray(originArray, addedElement) {
+  var originArray = originArray.unshift(addElement)
+  return originArray
 }
